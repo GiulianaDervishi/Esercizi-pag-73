681505c22daf0b3d33b41ed8b8da166b15dc1254
GiulianaDervishi <74350458+GiulianaDervishi@users.noreply.github.com>
2020-11-26 10:27:00 +0100

    Update es1.py

--- es1.py
+++ es1.py
@@ -9,15 +9,3 @@ if voto1> voto2:
     print("Il vincitorere è il primo candidato")
 else : 
     print("Ha vinto il secondo candidato")
-
-27.
-somma = 0
-conto =0
-while True:
-    numero = int(input("scrivi numero veicoli per giorno: "))
-    if numero == 0: 
-        break
-    else :
-        somma += numero
-        conto+= 1
-print("Il totale dei veicoli per ", conto, "giorni è: %s" % somma)
