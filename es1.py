0a1714559b26b27165134f5c82b3108854fb1767
GiulianaDervishi <74350458+GiulianaDervishi@users.noreply.github.com>
2020-11-26 10:25:09 +0100

    Update es1.py

--- es1.py
+++ es1.py
@@ -1,11 +1,11 @@
-votoa = int(input("Voti del primo candidato: "))
-votob = int(input("Voti del secondo candidato: "))
-somma = votoa + votob
-percentualea  = (votoa/somma)*100
-percentualeb = (votob/somma)*100
+voto1 = int(input("Voti del primo candidato: "))
+voto2 = int(input("Voti del secondo candidato: "))
+somma = voto1 + voto2
+percentuale1  = (votoa/somma)*100
+percentuale2 = (votob/somma)*100
 
-print(" Le percentuali sono :  Il primo", percentualea, "%    Il secondo", percentualeb, "%")
-if votoa> votob: 
+print(" Le percentuali sono :  Il primo", percentuale1, "%    Il secondo", percentuale2, "%")
+if voto1> voto2: 
     print("Il vincitorere è il primo candidato")
 else : 
     print("Ha vinto il secondo candidato")
